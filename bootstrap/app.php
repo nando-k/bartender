c09309384076b485ef9f02ca7c171129f67d5a71
Abraham <yanaab@epsas.com>
2026-08-24 16:16:49 -0400

    - se adiciona las rutas en bootstrap\app.php - configuración de la BD, desde hosting de sdaya

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,13 +4,36 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-    )
+		then: function () {
+			// módulo de seguridad
+			Route::middleware('api')
+				->prefix('api')
+				->group(base_path('routes/seguridad.php'));
+
+			// módulo de empleado
+			Route::middleware('api')
+				->prefix('api')
+				->group(base_path('routes/empleado.php'));
+
+			// módulo de almacén
+			Route::middleware('api')
+				->prefix('api')
+				->group(base_path('routes/almacen.php'));
+
+			// módulo de parametro
+			Route::middleware('api')
+				->prefix('api')
+				->group(base_path('routes/parametro.php'));
+		},
+
+	)
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
